b20470d19aad7f5f484f74a539125686041a27b3
balckowl <shiggyjr1492@gmail.com>
2025-02-02 22:15:17 +0900

    feat: クイズの作成画面を実装

--- src/components/molecules/ChoiceInput/ChoiceInput.tsx
+++ src/components/molecules/ChoiceInput/ChoiceInput.tsx
@@ -1,3 +1,5 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ChangeEvent, Ref } from "react";
 
 type FieldProps = {
@@ -9,23 +11,33 @@ type FieldProps = {
 };
 
 type Props = {
-  isChecked: boolean;
   handleCheckBoxChange: () => void;
+  handleRemoveOption: () => void;
   placeholder: string;
   field?: FieldProps;
+  option: OptionType;
+  isRemovable: boolean;
+};
+
+type OptionType = {
+  id: number;
+  text: string;
+  isCorrect: boolean
 };
 
 export default function ChoiceInput({
   field,
-  isChecked,
   handleCheckBoxChange,
+  handleRemoveOption,
   placeholder,
+  option,
+  isRemovable,
 }: Props) {
   return (
-    <div className={`flex items-center mb-2 border border-black rounded-xl`}>
+    <div className={`flex items-center border border-black rounded-xl`}>
       <div className="w-[45px] h-[40px] grid place-content-center border-black border-r">
         <input
-          checked={isChecked}
+          checked={option.isCorrect}
           onChange={handleCheckBoxChange}
           type="checkbox"
           className="m-2 border-black border-l size-[15px]"
@@ -37,6 +49,15 @@ export default function ChoiceInput({
         className="flex-1  p-2 focus:outline-none rounded-xl"
         placeholder={placeholder}
       />
+      <div className="w-[45px] h-[40px] grid place-content-center">
+        {isRemovable && !option.isCorrect && (<button
+          type="button"
+          onClick={handleRemoveOption}
+          className="ml-2 p-1 rounded"
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>)}
+      </div>
     </div>
   );
 }
---
+++ src/components/organisms/QuizBuilder/QuizBuilder.tsx
@@ -0,0 +1,89 @@
+import { ChoiceInput } from "@/components/molecules/ChoiceInput";
+import { Controller, useFieldArray, UseFormReturn } from "react-hook-form";
+import { Button } from "../../atoms/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { QuizFormData } from "@/store/useQuizStore";
+
+
+export default function QuizBuilder({ Form }: { Form: UseFormReturn<QuizFormData> }) {
+
+  const { control, watch, setValue, formState: { errors } } = Form
+  const { fields, append, remove } = useFieldArray({ control, name: "options" });
+  const options = watch("options");
+
+  const handleCheckboxChange = (id: number) => {
+    setValue(
+      "options",
+      options.map((option) => ({
+        ...option,
+        isCorrect: option.id === id,
+      }))
+    );
+  };
+
+  const addOption = () => {
+    if (fields.length < 4) {
+      append({ id: Date.now(), text: "", isCorrect: false });
+    }
+  };
+
+  const removeOption = (index: number) => {
+    if (fields.length > 2) {
+      remove(index);
+    }
+  };
+
+  return (
+    <form className="space-y-2">
+      <Controller
+        name="question"
+        control={control}
+        render={({ field }) => (
+          <>
+            <input
+              {...field}
+              className="w-full p-2 border-b border-black focus:outline-none text-[1.3rem]"
+              placeholder="問題文"
+            />
+            {errors.question && <p className="text-red-500">{errors.question.message}</p>}
+          </>
+        )}
+      />
+      {fields.map((option, index) => (
+        <Controller
+          key={option.id}
+          name={`options.${index}.text`}
+          control={control}
+          render={({ field }) => (
+            <>
+              <ChoiceInput
+                field={field}
+                placeholder={`${index + 1}目の選択肢`}
+                option={options[index]}
+                handleCheckBoxChange={() => handleCheckboxChange(options[index].id)}
+                handleRemoveOption={() => removeOption(index)}
+                isRemovable={fields.length > 2}
+              />
+              {errors.options && <p className="text-red-500 text-[0.8rem]">{errors.options?.[index]?.text?.message}</p>}
+            </>
+          )}
+        />
+      ))}
+      {errors.options && <p className="text-red-500">{errors.options.message}</p>}
+      {fields.length < 4 && (
+        <div className="flex justify-center">
+          <Button
+            type="button"
+            onClickFn={() => addOption()}
+            size="sm"
+          >
+            <div className="flex items-center gap-2">
+              <FontAwesomeIcon icon={faPlus} />選択肢を追加する
+            </div>
+          </Button>
+        </div>
+      )}
+    </form>
+  );
+}
