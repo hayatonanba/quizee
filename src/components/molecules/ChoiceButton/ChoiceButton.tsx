87801d407f57a4226112390d163d8dea75f0a960
難波隼人 <hayatonanba@gmail.com>
2025-03-04 02:39:33 +0900

    chore: ホームボタンのスタイルを修正

--- src/components/molecules/ChoiceButton/ChoiceButton.tsx
+++ src/components/molecules/ChoiceButton/ChoiceButton.tsx
@@ -8,7 +8,7 @@ export default function ChoiceButton({ onClickFn, text, isLoading }: Props) {
   return (
     <button
       type="button"
-      className="w-full rounded-full border border-black py-2 text-[1rem]"
+      className="w-full rounded-full border border-black py-2 text-[1rem] transition-all duration-200 hover:scale-[1.03]"
       onClick={onClickFn}
       disabled={isLoading}
     >
