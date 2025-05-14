61dae34f0ca5da37afa24bd1ec5d55c6107089a6
難波隼人 <hayatonanba@gmail.com>
2025-05-14 14:48:44 +0900

    fix: コンポーネントの切り分けを修正

--- src/components/molecules/SettingText/SettingText.tsx
+++ src/components/molecules/SettingText/SettingText.tsx
@@ -1,13 +1,28 @@
-import type { ReactNode } from "react";
+import { Button } from '@/components/atoms/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
-  children: ReactNode
+  inputName: string;
+  placeholder: string
 }
 
-export default function SettingText({children}: Props) {
+export default function SettingText({inputName, placeholder}: Props) {
     return (
-        <div>
-          {children}
+      <div className='w-[500px]'>
+        <div className='h-[80px] bg-gray-100 p-3'>
+          <h1 className='font-bold'>{inputName}</h1>
+          <label className="flex items-center border-black border-b py-1">
+            <input type="text" className="w-full appearance-none border-none bg-transparent text-gray-700 focus:outline-none" placeholder={placeholder} />
+            <FontAwesomeIcon icon={faPen} />
+          </label>         
         </div>
+        <div className="flex justify-between bg-gray-200 p-2">
+          <small  className='flex items-center text-gray-500'>32文字まで入力可能です。</small>
+          <Button type='button' size='sm'>
+             保存する
+          </Button>
+        </div>
+      </div>
     );
 }
