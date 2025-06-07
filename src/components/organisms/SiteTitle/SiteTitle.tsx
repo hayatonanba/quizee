f8c467a5bac70a6d7003a6b239f3da098152b2b1
balckowl <shiggyjr1492@gmail.com>
2025-06-07 20:10:25 +0900

    feat: トップページに規約のリンクを設置

--- src/components/organisms/SiteTitle/SiteTitle.tsx
+++ src/components/organisms/SiteTitle/SiteTitle.tsx
@@ -1,5 +1,5 @@
 export default function SiteTitle() {
     return (
-        <h1 className="font-bold text-[4rem]">Quizee</h1>
+        <h1 className="text-center font-bold text-[4rem]">Quizee</h1>
     );
 }
--- src/components/templates/topPageTemplate/topPageTemplate.tsx
+++ src/components/templates/topPageTemplate/topPageTemplate.tsx
@@ -5,19 +5,28 @@ import SiteTitle from "@/components/organisms/SiteTitle/SiteTitle";
 export default function TopPageTemplate() {
   return (
     <div className="grid h-screen place-content-center">
-      <div className="mb-[160px]">
-        <div className="mb-[15px]">
-          <SiteTitle />
-        </div>
-        <div className="text-center">
-          <form action={async () => {
-            "use server"
-            await signIn("google", { redirectTo: "/home" })
-          }}>
-            <Button type="submit" size="md">
-              ログイン
-            </Button>
-          </form>
+      <div className="container mx-auto px-5">
+        <div className="mb-[160px]">
+          <div className="mb-[15px]">
+            <SiteTitle />
+          </div>
+          <div className="text-center">
+            <form className="mb-8" action={async () => {
+              "use server"
+              await signIn("google", { redirectTo: "/home" })
+            }}>
+              <Button type="submit" size="md">
+                ログイン
+              </Button>
+            </form>
+          </div>
+          <p className="text-sm">
+            ログイン後、本サイトの
+            <a target="_blank"
+              rel="noreferrer"
+              href="https://flawless-blue-360.notion.site/20b1bdc7ca9080be81f7eee5769d4bd4" className="border-black border-b">規約とポリシー</a>
+            に同意したものとみなされます。
+          </p>
         </div>
       </div>
     </div>
