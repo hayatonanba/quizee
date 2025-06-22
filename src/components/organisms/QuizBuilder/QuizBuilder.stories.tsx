49d7e591f36c2fbb2f05dbf7fc6944a28c794d6e
難波隼人 <hayatonanba@gmail.com>
2025-06-22 23:29:38 +0900

    fix: storybookのエラーを修正

--- src/components/organisms/QuizBuilder/QuizBuilder.stories.tsx
+++ src/components/organisms/QuizBuilder/QuizBuilder.stories.tsx
@@ -5,19 +5,20 @@ import { useQuizStore } from '@/store/useQuizStore';
 type T = typeof QuizBuilder;
 
 export default {
-    title: 'organisms/QuizBuilder',
-    component: QuizBuilder,
+  title: 'organisms/QuizBuilder',
+  component: QuizBuilder,
 } satisfies Meta<T>;
 
+const QuizBuilderWrapper: React.FC = () => {
+  const form = useQuizStore().newForm();
+
+  return (
+    <div className="w-3/5">
+      <QuizBuilder Form={form} />
+    </div>
+  );
+};
+
 export const Default: StoryObj<T> = {
-  render:() => {
-
-    const Form = useQuizStore().newForm()
-
-    return (
-      <div className='w-3/5'>
-        <QuizBuilder Form={Form}/>
-      </div>
-    )
-  }
+  render: () => <QuizBuilderWrapper />,
 };
