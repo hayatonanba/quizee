f2394ef26aa8c22d1a33a22debc41761cae0f8df
balckowl <shiggyjr1492@gmail.com>
2025-06-22 22:48:51 +0900

    feat: 未認証時、ページをブロック

--- src/app/(pages)/quiz/new/page.tsx
+++ src/app/(pages)/quiz/new/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
 import CreateQuizTemplate from "@/components/templates/createQuizTemplate/createQuizTemplate";
 
-export default function Page() {
+export default async function Page() {
+
+  const session = await auth()
+
+  if (!session) {
+    return <div>認証してください。</div>
+  }
+
   return (
-      <CreateQuizTemplate />
+    <CreateQuizTemplate />
   );
 }
